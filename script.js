c5959e0b793bef1e209a67cdc7ed392326e4aa2d
Braian de Carvalho Gomes <gomes.braian88@gmail.com>
2023-09-13 12:54:04 +1000

    updating contact

--- script.js
+++ script.js
@@ -28,3 +28,26 @@ var swiper = new Swiper(".mySwiper", {
     dynamicBullets: true,
   },
 });
+
+/*--------------Fixando Procura-------------------*/
+
+let allSection = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  allSection.forEach((sec) => {
+    let backTop = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (backTop >= offset && backTop < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*= " + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+;}

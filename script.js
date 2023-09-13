78948d59904176d655a307d33e04e4b46528218d
Braian de Carvalho Gomes <gomes.braian88@gmail.com>
2023-09-13 11:37:40 +1000

    updating

---
+++ script.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 200,
+  loop: true,
+  centeredSlides: false,
+  slidesPerGroupSkip: 1,
+  grabCursor: true,
+  keyboard: {
+    enabled: true,
+  },
+  breakpoints: {
+   
+    769: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
